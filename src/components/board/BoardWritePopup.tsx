1b50a22148834ca25ea17c9b982d13fd91cf4248
yoonui <yoony0612@ajou.ac.kr>
2024-09-21 15:35:03 +0900

    feature: 첨부파일 업로드

--- src/components/board/BoardWritePopup.tsx
+++ src/components/board/BoardWritePopup.tsx
@@ -1,4 +1,5 @@
 // import { Dispatch, SetStateAction } from "react";
+import { useRef } from "react";
 import { TbPhotoVideo } from "react-icons/tb";
 
 const BoardWritePopup = ({
@@ -8,6 +9,8 @@ const BoardWritePopup = ({
   show: boolean;
   // setShow: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const imgUploadRef = useRef<HTMLInputElement>(null);
+
   return show ? (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 text-center">
       <div className="w-1/3 bg-white rounded-md">
@@ -18,7 +21,21 @@ const BoardWritePopup = ({
           <TbPhotoVideo size="80" className="w-full" />
           <div className="text-xl">사진과 동영상을 여기에 끌어다 놓으세요</div>
           <div className="flex justify-center">
-            <button className="flex justify-center p-2 bg-blue-400 w-[140px] rounded-md text-white font-bold">
+            <input
+              ref={imgUploadRef}
+              type="file"
+              name="images"
+              className="hidden"
+              // onChange={onSelectFile}
+              accept=".png, .jpg, image/*, .mp4"
+            />
+            <button
+              className="flex justify-center p-2 bg-blue-400 w-[140px] rounded-md text-white font-bold"
+              onClick={(e) => {
+                e.preventDefault();
+                if (imgUploadRef.current) imgUploadRef.current.click();
+              }}
+            >
               컴퓨터에서 선택
             </button>
           </div>
